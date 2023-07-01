dd7408564885f514466ec6792309ef6f6997ed9a
nesahe <neisha695@gmail.com>
2023-07-01 23:05:42 +0300

    add error page

--- client/src/compound/Error/index.tsx
+++ client/src/compound/Error/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import Robot from './images/robot.svg';
+
+import styles from './index.module.scss';
+
+import { Link } from 'react-router-dom';
+
 const Error = () => {
     return (
-        <div>
-            error
-        </div>
+        <section className={styles.root}>
+            <div className={styles.root__body}>
+                <div className={styles.root__image}>
+                    <img src={Robot} alt="robot" />
+                </div>
+                <div className={styles.root__body__text}>
+                    <h2 className={styles.root__title}>404</h2>
+                    <div className={styles.root__text}>Not found</div>
+                    <Link className={styles.root__link} to="/">Home</Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
